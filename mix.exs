ec3415b09568125474e1bc5fa634b3a2b5a90892
Ryan Schmukler <rschmukler@gmail.com>
2017-04-25 16:39:11 -0400

    Release 1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Rox.Mixfile do
 
   def project do
     [app: :rox,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
